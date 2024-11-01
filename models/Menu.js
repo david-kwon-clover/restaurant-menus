bcbb4b6f5012943fbe6db283a291d748ecbf5c16
David Kwon <david.kwon@clover.com>
2024-11-01 13:08:26 -0600

    Add Menu model in Menu.js

--- models/Menu.js
+++ models/Menu.js
@@ -2,5 +2,8 @@ const { sequelize } = require('../db');
 const { Sequelize, DataTypes } = require('sequelize');
 
 // TODO - create a Menu model
+const Menu = sequelize.define("Menu", {
+    title: DataTypes.STRING
+})
 
 module.exports = {Menu};
